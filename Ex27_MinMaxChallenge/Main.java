3515a3011f1f546a125d3cd1e9038da7d5794184
Joseph Gale <24618839+josephgale@users.noreply.github.com>
2019-05-09 21:41:43 -0500

    Update Main.java

--- Ex27_MinMaxChallenge/Main.java
+++ Ex27_MinMaxChallenge/Main.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+
+public class MinMaxChallenge {
+    public static void main(String[] args){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Enter an integer (type invalid number to quit)");
+            boolean validInput = scanner.hasNextInt();
+            if(!validInput){
+                break;
+            }
+            int userInput = scanner.nextInt();
+
+            if(userInput>max){
+                max = userInput;
+            }
+            if(userInput<min){
+                min = userInput;
+            }
+
+        }
+        scanner.close();
+        System.out.println("Max integer entered: " + max);
+        System.out.println("Min integer entered: " + min);
+    }
+}
 
