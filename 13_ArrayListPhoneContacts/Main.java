aa7a56f39dd7c22118c75ab5828009890fec10e8
Joseph Gale <24618839+josephgale@users.noreply.github.com>
2019-05-15 17:12:04 -0500

    Create Main.java

---
+++ 13_ArrayListPhoneContacts/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        MobilePhone joePhone = new MobilePhone(5126668713L);
+        joePhone.pushContactsButton();
+
+    }
+}
