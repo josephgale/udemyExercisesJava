114888e69ba2dfb0e611d1680f7f79f794c1c68e
Joseph Gale <24618839+josephgale@users.noreply.github.com>
2019-05-23 16:03:50 -0500

    Create Main.java

---
+++ 16_InnerClassesMusicPlayList/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Album myAlbum = new Album("Beatles");
+        myAlbum.addSong("Yellow Submarine",120);
+        myAlbum.addSong("Twist and Shout",200);
+
+
+        Playlist myPlayList = new Playlist("Friday Party");
+
+        myPlayList.addAlbum(myAlbum);
+        myPlayList.addSong("Beatles","Yellow Submarine");
+        myPlayList.addSong("Beatles","Twist and Shout");
+        myPlayList.run();
+    }
+
+}
