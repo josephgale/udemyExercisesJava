b6198d78f593cd28294f4053b79c09e0912ead6f
Joseph Gale <24618839+josephgale@users.noreply.github.com>
2019-05-27 15:57:02 -0500

    Create Baseball.java

---
+++ GuidedExercises&Tutorials/Generics/Baseball.java
@@ -0,0 +1,11 @@
+public class BaseballPlayer extends Player {
+
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+
+    //not required to override all methods like interface
+
+
+
+}
